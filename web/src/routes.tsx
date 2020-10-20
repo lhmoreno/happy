d6796e3f22fd2943acdb9b988493af73b1a03a50
lhmoreno <lhmorenoti@hotmail.com>
2020-10-20 20:29:20 -0300

    Add routes dashboard

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -7,16 +7,27 @@ import OrphanagesMap from './pages/OrphanagesMap';
 import Orphanage from './pages/Orphanage';
 import CreateOrphanage from './pages/CreateOrphanage';
 import Login from './pages/Login';
+import RegisteredOrphanages from './pages/RegisteredOrphanages';
+import PendingOrphanages from './pages/PendingOrphanages';
+import DeleteOrphanage from './pages/DeleteOrphanage';
+import EditOrphanage from './pages/EditOrphanage';
+import OrphanagePending from './pages/OrphanagePending';
 
 function Routes() {
   return (
     <BrowserRouter>
         <Switch>
           <Route path="/" exact component={Landing} />
-          <Route path="/app" component={OrphanagesMap} />
+          <Route path="/map" component={OrphanagesMap} />
           <Route path="/login" component={Login} />
           <Route path="/orphanages/create" component={CreateOrphanage} />
           <Route path="/orphanages/:id" component={Orphanage} />
+
+          <Route path="/dashboard/delete/:id" component={DeleteOrphanage} />
+          <Route path="/dashboard/edit/:id" component={EditOrphanage} />
+          <Route path="/dashboard/pending/:id" component={OrphanagePending} />
+          <Route path="/dashboard/orphanages" component={RegisteredOrphanages} />
+          <Route path="/dashboard/pending" component={PendingOrphanages} />
         </Switch>
     </BrowserRouter>
   );

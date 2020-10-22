2b59e76ac3ab2b9ce64281d7a1e0c089c8f6f7d2
lhmoreno <lhmorenoti@hotmail.com>
2020-10-21 22:11:44 -0300

    Organize routes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -3,14 +3,27 @@ import multer from 'multer';
 
 import uploadConfig from './config/upload';
 
-import OrphanagesController from './controllers/OrphanagesController';
+import OrphanagesPendingController from './controllers/OrphanagesPendingController';
+import OrphanagesRegisteredController from './controllers/OrphanagesRegisteredController';
+
+import UsersController from './controllers/UsersController';
+import SessionsController from './controllers/SessionsController';
 
 const routes = Router();
 const upload = multer(uploadConfig);
 
-routes.get('/orphanages', OrphanagesController.index);
-routes.get('/orphanages/:id', OrphanagesController.show);
+// ROTAS ABERTAS
+routes.get('/orphanages', OrphanagesRegisteredController.index); // Busca todos os orfanatos cadastrados
+routes.get('/orphanage/:id', OrphanagesRegisteredController.show); // Busca apenas um orfanato cadastrado por id
+routes.post('/create/orphanage', upload.array('images'), OrphanagesPendingController.create); // Cria um orfanato para ser aprovado pelo adm
+routes.post('/login', SessionsController.create); // Loga um adm
 
-routes.post('/orphanages', upload.array('images'), OrphanagesController.create);
+// ROTAS RESTRITAS
+routes.get('/orphanages/pending', SessionsController.show, OrphanagesPendingController.index); // Mostra todos os orfanatos pendentes para o adm
+routes.put('/confirm/orphanage/:id', SessionsController.show, OrphanagesPendingController.update); // Adm confirma a criação do orfanato
+routes.delete('/delete/orphanage/:id', SessionsController.show, OrphanagesPendingController.destroy); // Adm deleta orfanato
+
+// ROTAS ESCONDIDAS
+routes.post('/create/user', UsersController.create); // Cria um adm
 
 export default routes;

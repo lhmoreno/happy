b12a305409cdcefe0c7704b1ad04afb06181f0e4
lhmoreno <lhmorenoti@hotmail.com>
2020-10-22 22:25:03 -0300

    Context provider auth

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,11 +3,14 @@ import React from 'react';
 import './app.css';
 import 'leaflet/dist/leaflet.css';
 
+import { AuthProvider } from './contexts/auth';
 import Routes from './routes';
 
 function App() {
   return (
-    <Routes />
+    <AuthProvider>
+      <Routes />
+    </AuthProvider>
   );
 }
 

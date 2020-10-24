a18f141a08f586bbd92bdad88421fca45da17263
lhmoreno <lhmorenoti@hotmail.com>
2020-10-23 22:36:29 -0300

    New route edit orphanage

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -21,7 +21,9 @@ routes.post('/login', SessionsController.create); // Loga um adm
 // ROTAS RESTRITAS
 routes.get('/orphanages/pending', SessionsController.show, OrphanagesPendingController.index); // Mostra todos os orfanatos pendentes para o adm
 routes.put('/confirm/orphanage/:id', SessionsController.show, OrphanagesPendingController.update); // Adm confirma a criação do orfanato
-routes.delete('/delete/orphanage/:id', SessionsController.show, OrphanagesPendingController.destroy); // Adm deleta orfanato
+routes.delete('/cancel/orphanage/:id', SessionsController.show, OrphanagesPendingController.destroy); // Adm recusa a criação do orfanato
+routes.put('/edit/orphanage/:id', SessionsController.show, upload.array('images'), OrphanagesRegisteredController.update); // Adm edita o orfanato
+routes.delete('/delete/orphanage/:id', SessionsController.show, OrphanagesRegisteredController.destroy); // Adm deleta orfanato
 
 // ROTAS ESCONDIDAS
 routes.post('/create/user', UsersController.create); // Cria um adm

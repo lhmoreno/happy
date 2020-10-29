3c09740c29eaf11650347f4c69989da375b77b82
lhmoreno <lhmorenoti@hotmail.com>
2020-10-28 21:22:50 -0300

    Add Onboarding if first time

--- mobile/src/routes.tsx
+++ mobile/src/routes.tsx
@@ -1,19 +1,42 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { View } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-const {Navigator, Screen} = createStackNavigator();
+const { Navigator, Screen } = createStackNavigator();
 
+import Onboarding from './pages/Onboarding';
 import OrphanagesMap from './pages/OrphanagesMap';
 import OrphanageDetails from './pages/OrphanageDetails';
 
 import SelectMapPosition from './pages/CreateOrphanage/SelectMapPosition';
 import OrphanageData from './pages/CreateOrphanage/OrphanageData';
 import Header from './components/Header';
+import ContextOnboarding from './contexts/onboarding';
 
 export default function Routes(){
-  return(
+  const [loading, setLoading] = useState(true);
+  const { onboarding, setOnboarding } = useContext(ContextOnboarding)
+
+  useEffect(() => {
+    (async () => {
+      const onboardingStorage = await AsyncStorage.getItem('onboarding');
+
+      if (!onboardingStorage || onboardingStorage === 'true') {
+        setOnboarding(true);
+      }
+
+      setLoading(false);
+    })()
+  }, []);
+
+  if (loading) {
+    return <View />
+  }
+
+  return (
     <NavigationContainer>
       <Navigator 
         screenOptions={{ 
@@ -22,6 +45,7 @@ export default function Routes(){
             backgroundColor: '#F2F3F5' 
           } 
         }}
+        initialRouteName={onboarding ? 'Onboarding' : 'OrphanagesMap'}
       >
         <Screen 
           name="OrphanagesMap" 
@@ -52,6 +76,13 @@ export default function Routes(){
             header: () => <Header title="Informe os dados" />
           }}
         />
+
+        {onboarding && (
+          <Screen 
+            name="Onboarding" 
+            component={Onboarding} 
+          />
+        )}
       </Navigator>
     </NavigationContainer>
   );

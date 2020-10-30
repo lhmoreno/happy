2a9622c93bf30116e2b5dcc9e45f6ea171c52cb6
lhmoreno <lhmorenoti@hotmail.com>
2020-10-29 21:24:18 -0300

    Add guide location

--- mobile/src/pages/OrphanagesMap.tsx
+++ mobile/src/pages/OrphanagesMap.tsx
@@ -47,7 +47,7 @@ export default function OrphanagesMap() {
   }
 
   function handleNavigateToCreateOrphanage() {
-    navigation.navigate('SelectMapPosition');
+    navigation.navigate('SelectMapPosition', { location });
   }
 
   if (location.latitude === 0) {

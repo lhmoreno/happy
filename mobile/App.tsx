555718c1683490c446803b23d8ab8c295db5dca5
lhmoreno <lhmorenoti@hotmail.com>
2020-10-29 21:23:41 -0300

    New font style

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 
 import { useFonts } from 'expo-font';
-import { Nunito_600SemiBold, Nunito_700Bold, Nunito_800ExtraBold } from '@expo-google-fonts/nunito';
+import { Nunito_600SemiBold, Nunito_700Bold, Nunito_800ExtraBold, Nunito_400Regular } from '@expo-google-fonts/nunito';
 
 import Routes from './src/routes';
 import { ContextProvider } from './src/contexts/onboarding';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
+    Nunito_400Regular,
     Nunito_600SemiBold,
     Nunito_700Bold,
     Nunito_800ExtraBold,
